1c6ae7822d9020b73d866722c8a348db9017feb2
unknown <susan.k.izawa@gmail.com>
2024-02-11 21:02:33 -0300

    Reapply "test: Implementing unittests with pytest for tag creator controller and validators"
    This reverts commit f906eab78654348ce11cdd8cbb7bddd77000dd12.

---
+++ src/validators/tag_creator_validator_test.py
@@ -0,0 +1,19 @@
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
+from .tag_creator_validator import tag_creator_validator
+
+class MockRequest:
+    def __init__(self, json) -> None:
+        self.json = json
+
+def test_tag_creator_validator():
+    mock_request = MockRequest(json={ "product_code": "12345" })
+    tag_creator_validator(mock_request)
+
+def test_tag_creator_validator_with_error():
+    mock_request = MockRequest(json={ "product_code": 12345 })
+
+    try:
+        tag_creator_validator(mock_request)
+        assert False
+    except Exception as exception:
+        assert isinstance(exception, HttpUnprocessableEntityError)
